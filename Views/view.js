19312e6405f74736abd17f26ac82b458acff1289
student <student@MAC5-28.local>
2017-01-13 09:55:00 -0800

    Change format

--- Views/view.js
+++ Views/view.js
@@ -1,13 +1,15 @@
 $("#submit").on("click", function() {
 	var name = $("#name").val();
 	var breed = $("#breed").val();
+	var select = $("#select").val();
 	
 	var objForTransport = {
 		name: name,
-		breed: breed
+		breed: breed,
+		select: select
 
 	}
 
-	console.log(objForTransport.name);
+	console.log(objForTransport);
 
 })
