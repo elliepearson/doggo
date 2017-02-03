f66d4745afca640ee7f425cc3a8f675a885b31dc
hellopi314159265 <mathew.shaham@newwestcharter.org>
2017-02-03 11:01:52 -0800

    Finished basic local storage

---
+++ Views/view.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+
+
+//printing the object
+$("#submit").on("click", function() {
+	var name = $("#add_name").val();
+	var breed = $("#breed").val();
+	var select = $("#select").val();
+	
+	var objectSubmit = {
+		name: name,
+		breed: breed,
+		select: select
+
+	}
+
+console.log(objectSubmit);
+
+localStorage.setItem('objectSubmit', JSON.stringify(objectSubmit));
+objectSubmit = JSON.parse(localStorage.getItem('objectSubmit'));
+document.getElementById("nameSubmit").innerHTML = objectSubmit.name;
+document.getElementById("breedSubmit").innerHTML = objectSubmit.breed;
+document.getElementById("selectSubmit").innerHTML = objectSubmit.select;
+
+})
+
+
+})
+
+ if (localStorage.getItem('objectSubmit') === null){ //set name if no name is set
+	localStorage.setItem('objectSubmit', JSON.stringify('objectSubmit'));
+}
+
+
+objectSubmit = JSON.parse(localStorage.getItem('objectSubmit'));
+document.getElementById("nameSubmit").innerHTML = objectSubmit.name;
+document.getElementById("breedSubmit").innerHTML = objectSubmit.breed;
+document.getElementById("selectSubmit").innerHTML = objectSubmit.select;
+
+
+
